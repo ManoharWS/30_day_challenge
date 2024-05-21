7e0bbf5f4349f9f4c9b9fb88dd2be578363fd8cf
ManoharWS <145865861+ManoharWS@users.noreply.github.com>
2024-05-20 20:56:37 -0700

    Update javscriptprogram1.js
    Signed-off-by: ManoharWS <145865861+ManoharWS@users.noreply.github.com>

--- javscriptprogram1.js
+++ javscriptprogram1.js
@@ -1,1 +1,20 @@
 console.log("Started programming challenge")
+function myFunction(){
+  console.log("This is normal function");
+}
+myFunction(); //we need to call functions by their names to be executed/perfomed.
+
+function myFunction1(a){
+  return a*10;
+}
+myFunction1(10); //we need to call function by passing arguments or else it will return undefined
+
+const car = {
+  make:"honda",
+  model:"civic",
+  year:2013,
+  drive:function (){
+      console.log(`You are driving ${make}  ${model} ${year}`);
+  }
+};
+car.drive();
